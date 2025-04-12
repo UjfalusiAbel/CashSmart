38b471aaca7b51ee23eb596be8a86a4a53dc7515
Ujfalusi.Abel <abelujfalusi4@gmail.com>
2025-04-12 16:55:20 +0300

    Investment loose setup

--- CashSmart.Application/Models/InvestmentManagement/AddInvestment.cs
+++ CashSmart.Application/Models/InvestmentManagement/AddInvestment.cs
@@ -2,11 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 
 namespace CashSmart.Application.Models.InvestmentManagement
 {
     public class AddInvestment
     {
-        
+        public class AddInvestmentRequest : IRequest<Unit>
+        {
+            public Guid UserId;
+            public string Name { get; set; } = string.Empty;
+            public DateTime BuyDate { get; set; }
+            public float BuyPrice { get; set; }
+            public float Fee { get; set; }
+            public float Quantity { get; set; }
+        }
+
+        public class AddInvestmentRequestHandler : IRequestHandler<AddInvestmentRequest, Unit>
+        {
+            public async Task<Unit> Handle(AddInvestmentRequest request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }

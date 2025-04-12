8b5d1465539d1cd7e886fde279b576f266337250
Ujfalusi.Abel <abelujfalusi4@gmail.com>
2025-04-12 15:54:53 +0300

    setup

---
+++ CashSmart.Application/Models/InvestmentManagement/AddInvestment.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashSmart.Application.Models.InvestmentManagement
+{
+    public class AddInvestment
+    {
+        
+    }
+}
---
+++ CashSmart.Core/Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashSmart.Core.Models
+{
+    public class User
+    {
+        
+    }
+}
